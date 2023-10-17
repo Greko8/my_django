c03f8f0a5cc455db81b02adfa781452451fdc9ab
Ranif <faizrman@gmail.com>
2023-10-17 20:08:12 +0300

    second commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -5,6 +5,6 @@ class Sensor(models.Model):
     description = models.CharField(max_length=100)
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensors')
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temperature = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
